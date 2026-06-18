0f5993463f748ebc9548743680a428ff5bb978f7
Shaikh Al Nahian <sheikhalnahian@gmail.com>
2026-06-18 20:46:13 +0600

    prisma code for getting all users are added

--- src/script.ts
+++ src/script.ts
@@ -1,24 +1,29 @@
 import { prisma } from "./lib/prisma";
 
 async function main(){
-    const user = await prisma.user.create({
-        data:{
-            name: "Shaikh Al Nahian",
-            email: "nahian@gmail.com",
-            posts: {
-                create:{
-                    title:"hello word",
-                    content:"This is my first post",
-                    published: true,
-                },
-            },
-        },
+    // const user = await prisma.user.create({
+    //     data:{
+    //         name: "Shaikh Al Nahian",
+    //         email: "nahian@gmail.com",
+    //         posts: {
+    //             create:{
+    //                 title:"hello word",
+    //                 content:"This is my first post",
+    //                 published: true,
+    //             },
+    //         },
+    //     },
+    //     include:{
+    //         posts: true,
+    //     },
+    // })
+    // console.log("Created user:",user)
+    const allUsers = await prisma.user.findMany({
         include:{
-            posts: true,
+            posts:false,
         },
-    })
-    console.log("Created user:",user)
-    
+    });
+    console.log("All Users:",JSON.stringify(allUsers,null,2));
 }
 main().then(async()=>{
     await prisma.$disconnect();
