c686a695ac4800bdf108c670fd7e2350bca987f2
Shaikh Al Nahian <sheikhalnahian@gmail.com>
2026-06-18 20:32:41 +0600

    create user using prisma is completed

---
+++ src/script.ts
@@ -0,0 +1,29 @@
+import { prisma } from "./lib/prisma";
+
+async function main(){
+    const user = await prisma.user.create({
+        data:{
+            name: "Shaikh Al Nahian",
+            email: "nahian@gmail.com",
+            posts: {
+                create:{
+                    title:"hello word",
+                    content:"This is my first post",
+                    published: true,
+                },
+            },
+        },
+        include:{
+            posts: true,
+        },
+    })
+    console.log("Created user:",user)
+    
+}
+main().then(async()=>{
+    await prisma.$disconnect();
+}).catch(async(e)=>{
+    console.error(e);
+    await prisma.$disconnect()
+    process.exit(1);
+});
